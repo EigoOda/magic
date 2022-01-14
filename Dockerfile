bba42ce4cc1e73bfdb30eb267754899a6d4c18f6
dubs11kt <dubs11kt@gmail.com>
2022-01-14 09:33:25 +0900

    reset: repository

--- Dockerfile
+++
@@ -1,8 +0,0 @@
-# コードを実行するコンテナイメージ
-FROM alpine:3.10
-
-# アクションのリポジトリからコードファイルをコンテナのファイルシステムパス `/`にコピー
-COPY entrypoint.sh /entrypoint.sh
-
-# dockerコンテナが起動する際に実行されるコードファイル (`entrypoint.sh`)
-ENTRYPOINT ["/entrypoint.sh"]
--- main.go
+++ main.go
@@ -1,11 +1,37 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"fmt"
+	"log"
 
-func printHello() {
-	fmt.Println("Hello")
-}
+	meta_v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/tools/clientcmd"
+)
 
 func main() {
-	printHello()
+	client, err := newClient()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	pods, err := client.CoreV1().Pods("").List(context.TODO(), meta_v1.ListOptions{})
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for _, pod := range pods.Items {
+		fmt.Println(pod.Name)
+	}
+
+}
+
+func newClient() (kubernetes.Interface, error) {
+	kubeConfig, err := clientcmd.BuildConfigFromFlags("", clientcmd.RecommendedHomeFile)
+	if err != nil {
+		return nil, err
+	}
+
+	return kubernetes.NewForConfig(kubeConfig)
 }
